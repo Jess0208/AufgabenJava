fc68fddb2aff9817ffd6dbe219a3021e0f99740c
Jess0208 <73823283+Jess0208@users.noreply.github.com>
2020-11-10 12:01:32 +0100

    Create FitzQuark.java

---
+++ FitzQuark.java
@@ -0,0 +1,51 @@
+class FitzQuark
+{
+    static boolean OutlayIsNumber(int _Number)
+    {
+        if      (_Number%5 == 0 || _Number%7 == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    static void Outlay(int _Number)
+    {
+        if      (OutlayIsNumber(_Number))
+        {
+            System.out.println(_Number);
+        }
+        else if (_Number%5 == 0 && _Number%7 !=0)
+        {
+            System.out.println("Fitz");
+        }
+        else if (_Number%5 != 0 && _Number%7 ==0)
+        {
+            System.out.println("Quark");
+        }
+        else
+        {
+            System.out.println("FitzQuark");
+        }
+    }
+
+    static void IndexCounter()
+    {
+        int     IndexNumber = 1;
+
+        while   (IndexNumber < 101)
+        {
+            Outlay(IndexNumber);
+        }
+
+        IndexNumber++;
+    }
+
+    static void main(String[] args)
+    {
+        IndexCounter();
+    }
+}
